46684591efcf82aa1ef749c56bff3373c84fb36d
MajorTreble <92947792+MajorTreble@users.noreply.github.com>
2023-11-02 14:51:09 -0400

    Update $Display

--- metricstics/tests/app_test.py
+++ metricstics/tests/app_test.py
@@ -1,15 +1,21 @@
 """Test Module for the app"""
+import os
 from metricstics.src.app import App
 
 # pylint: disable=R0903
 # Too few public methods (1/2) (too-few-public-methods)
 
 
-class TestSimpleClass:
-    """Test class"""
-
+class TestApp:
+    """Test the App"""
+    
     def test_app(self):
         """Test App"""
+        
+        if os.environ.get('DISPLAY','') == '':
+            print('no display found. Using :0.0')
+            os.environ.__setitem__('DISPLAY', ':0.0')        
+        
         app = App()
         assert app.controller is not None
         assert app.view is not None
