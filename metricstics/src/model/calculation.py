b6171dcba7159502e32fc7be1be7fa9b0fd695c4
Protim <36602930+Protim-Proshun@users.noreply.github.com>
2023-11-05 18:10:18 -0500

    Update calculation.py add maximum and mode

--- metricstics/src/model/calculation.py
+++ metricstics/src/model/calculation.py
@@ -42,6 +42,58 @@ class Calculation:
 
         result["ArithmeticMean"] = pymath.sum(data) / size
 
+        def view_maximum(self, data, result):
+        """
+        Perform the Maximum.
+
+        Arg:
+            data(set): the data to work with
+            result(dic): store the result
+        """
+        data = data
+        maximum = data[0]
+        for i in data:
+            if i > maximum:
+                maximum = i
+        size = len(data)
+        if size == 0:
+            result["Maximum"] = 0
+            return
+
+        result["Maximum"] = maximum
+
+    def view_mode(self, data, result):
+        """
+        Perform the Mode.
+
+        Arg:
+            data(set): the data to work with
+            result(dic): store the result
+        """
+        data = data
+        max_count = 0
+        mode = 0
+
+        for i in data:
+            count = 0
+            current_number = i
+            for j in range(0, len(data)):
+                if current_number == data[j]:
+                    count = count + 1
+            if count > max_count:
+                max_count = count
+                mode = current_number
+
+        size = len(data)
+        if size == 0:
+            result["Mode"] = 0
+            return
+
+        if max_count == 1:
+            result["Mode"] = 0
+        else:
+            result["Mode"] = mode
+
     def calculate_mean_absolute_deviation(self, data, result):
         """
         Perform the Mean Absolute Deviation calculation.
