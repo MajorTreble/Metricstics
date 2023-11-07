d21d01390af3d4f756c8a3fe2aad74729fb1efe5
MajorTreble <92947792+MajorTreble@users.noreply.github.com>
2023-11-06 20:44:25 -0500

    formatting

--- metricstics/src/model/datareader.py
+++ metricstics/src/model/datareader.py
@@ -14,16 +14,18 @@ class DataReader:
         data_lst = []
         try:
             # pylint: disable=C0301
-            with open("/Users/hetdalal/Documents/GitHub/Metricstics/metricstics/src/model/random_data.txt",
-                      "r+", encoding="utf-8") as file1:
+            with open(
+                "/Users/hetdalal/Documents/GitHub/Metricstics/metricstics/src/model/random_data.txt",
+                "r+",
+                encoding="utf-8",
+            ) as file1:
                 for line in file1:
                     data = ""
                     for char in line:
-                        if char == '\n':
+                        if char == "\n":
                             data_lst.append(float(data))
                         else:
                             data += char
         except FileNotFoundError:
             print("File path not found.")
         return data_lst
-    
